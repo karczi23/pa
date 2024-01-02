e7ce1f2a62408fe44c9d83ee93fc1f38cdca4acf
karczi23 <karol.kozlinka@student.put.poznan.pl>
2024-01-02 14:05:21 +0100

    Container barebone, styled-components

---
+++ src/components/structure/Container.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 50%;
+    background-color: grey;
+    border-radius: 5em;
+    margin: 20px;
+`
+
+export { Container };
