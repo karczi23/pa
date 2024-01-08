210f9171fadf6bd3cff0e2c33ee36fd697cd4338
karczi23 <karol.kozlinka@student.put.poznan.pl>
2024-01-08 20:55:45 +0100

    formatting

--- src/components/chart/Chart.tsx
+++ src/components/chart/Chart.tsx
@@ -1,45 +1,51 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from 'react';
 import { ChartContext } from '../../context/ChartContext';
 import Bar from './Bar';
-import './css/chart.css'
+import './css/chart.css';
 import { ColorContext } from '../../context/ColorContext';
 import { ChartTextContainer, Container } from '../structure/Container';
 
 const Chart = (props) => {
+    const [isHovered, setHovered] = useState(false);
+    const [temperature, setTemperature] = useState(0);
 
-const [isHovered, setHovered] = useState(false)
-const [temperature, setTemperature] = useState(0)
+    const data = useContext(ChartContext);
+    const colors = useContext(ColorContext);
 
-const data = useContext(ChartContext)
-const colors = useContext(ColorContext)
+    const toggleHover = () => setHovered((prevState) => !prevState);
 
-const toggleHover = () => setHovered(prevState => !prevState);
-
-return (
-    <Container>
-    <div onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
-        {
-            isHovered ?
-            <div>
-                <ChartTextContainer/>
-                <div className='chartAreaHovered chartArea border-bottom border-left border-right'>
-                {
-                    data.map((value, index) => 
-                        <Bar key={index} setTemperature={setTemperature} temperature={value} color={colors[index]}/>
-                    )
-                }
-                </div>
-                <ChartTextContainer>
-                    {temperature && `Temperatura: ${temperature}°C`}
-                </ChartTextContainer>
+    return (
+        <Container>
+            <div onMouseEnter={toggleHover} onMouseLeave={toggleHover}>
+                {isHovered ? (
+                    <div>
+                        <ChartTextContainer />
+                        <div className="chartAreaHovered chartArea border-bottom border-left border-right border-radius">
+                            {data.map((value, index) => (
+                                <Bar
+                                    key={index}
+                                    setTemperature={setTemperature}
+                                    temperature={value}
+                                    color={colors[index]}
+                                />
+                            ))}
+                        </div>
+                        <ChartTextContainer>
+                            {temperature && `Temperatura: ${temperature}°C`}
+                        </ChartTextContainer>
+                    </div>
+                ) : (
+                    <div
+                        className="chartAreaUnhovered chartArea border-bottom border-left border-right border-radius"
+                        style={{
+                            backgroundImage: `linear-gradient(to right, ${colors.join(',')})`,
+                        }}>
+                        a
+                    </div>
+                )}
             </div>
-        
-            :
-            <div className='chartAreaUnhovered chartArea' style={{backgroundImage: `linear-gradient(to right, ${colors.join(',')})`}}>a</div>
-        }
-    </div>
-    </Container>
-  )
-}
+        </Container>
+    );
+};
 
 export default Chart;
