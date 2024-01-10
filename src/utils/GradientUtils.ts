d1120937529a554400423260ca34385e7d1d064b
karczi23 <karol.kozlinka@student.put.poznan.pl>
2024-01-10 21:51:43 +0100

    formatting

--- src/utils/GradientUtils.ts
+++ src/utils/GradientUtils.ts
@@ -1,14 +1,13 @@
 export const getColor = (temperature: number): string => {
+    const max = 35;
+    const min = 15;
+    const avg = (max + min) / 2;
 
-        const max = 35
-        const min = 15
-        const avg = (max + min) / 2
-
-        if (temperature <= avg) {
-            const percent = 1 - (avg - temperature) / (avg - min)
-            return `rgb(${255 * percent}, 255, 0)`
-        } else {
-            const percent = 1 - (temperature - avg) / (max - avg)
-            return `rgb(255, ${255 * percent}, 0)`
-        }
-}
+    if (temperature <= avg) {
+        const percent = 1 - (avg - temperature) / (avg - min);
+        return `rgb(${255 * percent}, 255, 0)`;
+    } else {
+        const percent = 1 - (temperature - avg) / (max - avg);
+        return `rgb(255, ${255 * percent}, 0)`;
+    }
+};
